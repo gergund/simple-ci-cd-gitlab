c049d6fd5f9de5f4f013d3b03dd56c8e824a3d74
Denys Gergun <gergund@gmail.com>
2020-01-28 15:37:47 +0200

    Fix

--- kubernetes/deploy-src/patch.bash
+++ kubernetes/deploy-src/patch.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sed -e 's/{VERSION}/$CI_COMMIT_SHA/' ../patch-magento2-nginx.json
-sed -e 's/{VERSION}/$CI_COMMIT_SHA/' ../patch-magento2-php-nginx.json
+sed -e 's/{VERSION}/$CI_COMMIT_SHA/' kubernetes/patch-magento2-nginx.json
+sed -e 's/{VERSION}/$CI_COMMIT_SHA/' kubernetes/patch-magento2-php-fpm.json
